7f5dee71b66ef14616d1f52b9682002bd34cc2a7
omimurkar <139636120+omimurkar@users.noreply.github.com>
2023-08-16 18:12:17 +0530

    Add files via upload

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Online Food Delivered To You</h2>
+      <p>
+        Choose your favorite meal and enjoy a delicious lunch or dinner at home.
+      </p>
+     
+    </section>
+  );
+};
+
+export default MealsSummary;
